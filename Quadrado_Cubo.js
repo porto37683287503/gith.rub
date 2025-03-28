27998d9bb81ce109426c9e06c82334395f13d80e
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-03-28 11:49:31 -0300

    feat(operadores-aritmeticos): adiciona cálculo de quadrado e cubo

---
+++ Quadrado_Cubo.js
@@ -0,0 +1,4 @@
+let quadrado = 20
+let cubo = 40
+console.log(" A area do quadrado é igual a " + quadrado * quadrado);
+console.log(" O valor do cubo é igual a "+ cubo * cubo * cubo);
---
+++ Soma.js
@@ -0,0 +1,4 @@
+let base = 20
+let altura = 50
+let area = base * altura 
+console.log(`A area do retângulo é igual a ${area}`)

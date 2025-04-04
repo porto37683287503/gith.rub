cae4b4ad6f36c316d57028de77e8aeea04b0f545
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-04-04 11:33:07 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ AND em acao.js
@@ -0,0 +1,8 @@
+let tenhoDinheiro = true
+let tenhoCartao = false
+
+if (tenhoDinheiro == true){
+    console.log(" Consigo pagar a conta")
+} else if( tenhoCartao == false){
+    console.log("  Não Consigo pagar a conta")
+}

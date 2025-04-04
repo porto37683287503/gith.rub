62a2e60bdd4820d3628e9396156a3ae052dd73d6
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-04-04 10:35:10 -0300

    feat(operadores-aritmeticos): adiciona cálculo da área do círculo

---
+++ Area do círculo.js
@@ -0,0 +1,4 @@
+let raio = 20
+let perimetro = 3.14 
+    let area =  3.14 * (raio * raio)
+    console.log(" A area do círculo é " + area)
--- Área do círculo.js
+++
@@ -1,4 +0,0 @@
-let raio = 20
-let perimetro = 3,14
-let area = 3,14 * (raio * raio)
-console.log( ´a

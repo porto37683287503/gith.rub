a7558d560a09c462155e26575d9e2de5fbbf911c
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-04-04 11:07:47 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

---
+++ Maior que 100.js
@@ -0,0 +1,11 @@
+let numb1 = 50 
+let numb2 = 200
+let numb3 = 100
+
+if( numb1 & numb2 & numb3 >= 100){
+    console.log(" Os números pasa de 100")
+} else {
+    console.log(" Os números passa de 100")
+    }
+    
+
--- Operadores Lógicos.js
+++ Operadores Lógicos.js
@@ -3,7 +3,7 @@ let numb2 = 2
 
 if(numb1 >0){
     console.log(" Os número são positivo")
-} else if(numb1 < 0){s
+} else if(numb1 < 0){
     console.log(" Os número são negatuivo")
 } else{
     console.log(" Os numéros são diferentes")

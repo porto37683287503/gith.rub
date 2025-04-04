b36e4c10c97da93ce11f68029a689707bcddafc1
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-04-04 09:55:58 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

---
+++ Pode votar.js
@@ -0,0 +1,8 @@
+let idade = 18
+switch (true) {
+    case idade >=18 && idade <70:
+        console.log(" Você pode votar!")
+        break;
+        default:
+            console.log(" Você não pode votar!")
+}
